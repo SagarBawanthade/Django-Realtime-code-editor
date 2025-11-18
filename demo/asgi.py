77dd7297308f1e9063d9338193465654fc4d92e3
SagarBawanthade <sagar.bawanthade2004@gmail.com>
2025-11-18 22:32:16 +0530

    Added Cursor Highlighting with username

--- demo/asgi.py
+++ demo/asgi.py
@@ -1,11 +1,16 @@
+# 
+
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 import editor.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": URLRouter(editor.routing.websocket_urlpatterns),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(editor.routing.websocket_urlpatterns)
+    ),
 })
--- editor/consumers.py
+++ editor/consumers.py
@@ -11,13 +11,25 @@ class CodeEditorConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
     async def receive(self, text_data):
+        data = json.loads(text_data)
         await self.channel_layer.group_send(
             self.room_name,
             {
                 'type': 'editor_update',
-                'text': text_data,
+                'text': data.get('text', ''),
+                'cursor': data.get('cursor', None),
+                'username': self.scope.get('user').username if self.scope.get('user') and self.scope.get('user').is_authenticated else 'Anonymous',
+
             }
         )
+    
+
 
     async def editor_update(self, event):
-        await self.send(text_data=event['text'])
+        msg = {
+        'text': event['text'],
+        'cursor': event.get('cursor'),
+        'username': event.get('username'),
+    }
+        await self.send(text_data=json.dumps(msg))
+
